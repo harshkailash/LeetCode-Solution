55725641724fc45e8d8da82d52219ddad75ac8c8
Harsh Kailash <harshkailash2000@gmail.com>
2022-08-31 13:12:37 +0530

    Time: 1 ms (70.01%), Space: 42.5 MB (61.67%) - LeetHub

---
+++ 86-partition-list/86-partition-list.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode lessHead = new ListNode(-1);
+        ListNode moreHead = new ListNode(-1);
+        ListNode less = lessHead;
+        ListNode more = moreHead;
+        ListNode node = head;
+        while (node !=null ){
+            if (node.val < x){
+                less.next = node;
+                less = less.next;
+            }
+            else{
+                more.next = node;
+                more = more.next;
+            }
+            node = node.next;
+        }//while node
+        more.next = null;
+        less.next = moreHead.next;
+        return lessHead.next;
+    }//partition
+}
